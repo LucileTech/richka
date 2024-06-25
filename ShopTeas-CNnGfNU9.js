45e2afab99b68a442d1982584d720a677e355378
LucileTech <lucile.t.tech@gmail.com>
2024-06-25 15:40:43 -0700

    Deploy with loading

---
+++ ShopTeas-CNnGfNU9.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-FVDhRmbp.js";const a=()=>s.jsx("div",{className:"business-header",id:"shop-teas",children:s.jsx("h1",{children:"Ukrainian brand Richka, continues the wonderful folk tradition of tea drinking and offers 4 author's blends of teas"})});export{a as default};
---
+++ TeasDescription-CElrnDzB.js
@@ -0,0 +1,10 @@
+import{j as e}from"./index-FVDhRmbp.js";import{I as i}from"./ImageAndDescription-DuXkhFYf.js";import{g as t}from"./getAssetPath-FP31T4yl.js";const l=()=>{const a=`A lot of us remember our childhood summer holidays at grandparents’ village house. 
+    With a forest, river and pond nearby. Every national holiday, according to the season, most of families had a tea ceremony. 
+    For example on Trinity, Ukrainians have tradition of decorating the home with greenery, which is believed to protect against evil spirits. 
+    So people collect thyme, cornflowers and wild herbs and brew it then. In winter or early spring, when the snow had not yet melted, they 
+    enjoyed fruit teas or raspberry buds and small chopped sprigs of wild and cultivated cherries. Such teas used to be common in every rural 
+    home. Any gatherings with tea, both with friends and in the family circle, began with tea.`,o=`It's no secret that tea is a popular selection for individuals who want to improve their health naturally. 
+    Unlike coffee and standard teas, most herbal teas are caffeine-free. They also feature nutrients, vitamins, minerals and antioxidants 
+    that vary depending on the herbal blend. Typical health benefits from drinking herbal tea include relaxation, pain reduction and the 
+    improvement of body systems such as digestive and immune. Here are the top health benefits and the herbal teas that you should consume 
+    to get them.`,n=[{src:t("/photographs/teaspageflowers.webp"),alt:"Tea Image"},{src:t("/photographs/teaspagetea.webp"),alt:"Tea Image"}];return e.jsx(e.Fragment,{children:e.jsx(i,{titletop:"a little bit of history...",contenttop:a,images:n,titlebottom:"What are the benefits of drinking herbal teas?",contentbottom:o,sectionIdTop:"teas-section-top",sectionIdBottom:"teas-section-bottom"})})};export{l as default};
