c678473341086287664ba2dc853b98bfdfad66ac
LucileTech <lucile.t.tech@gmail.com>
2024-06-26 17:26:56 -0700

    favorites

--- src/auth/UserContext.tsx
+++ src/auth/UserContext.tsx
@@ -3,14 +3,12 @@ import { createContext } from "react";
 interface UserContextType {
   isLoggedIn: boolean;
   currentUser: { email: string } | null;
-  favorites: number[]; // Assuming favorites are stored as product IDs
   removeUser: () => void;
 }
 
 const UserContext = createContext<UserContextType>({
   isLoggedIn: false,
   currentUser: null,
-  favorites: [],
   removeUser: () => {},
 });
 
